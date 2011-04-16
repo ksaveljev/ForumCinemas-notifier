da9793a83c481dc2b4e23df241eee34018e35f06
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2011-04-17 00:54:33 +0300

    Started on libforumcinemas to parse data

--- lib/libforumcinemas.rb
+++ lib/libforumcinemas.rb
@@ -1,3 +1,23 @@
-class ForumCinemas
+require 'nokogiri'
+require 'open-uri'
 
+class ForumCinemas
+  def initialize(forumcinemas_id)
+    @forumcinemas_id = forumcinemas_id
+  end
+
+  def valid?
+    document.nil?
+  end
+
+  def reload
+    @document = nil
+    document
+  end
+
+  private
+
+  def document
+    @document ||= Nokogori::HTML(open("http://www.forumcinemas.ee/Event/#{@forumcinemas_id}")) rescue nil
+  end
 end
